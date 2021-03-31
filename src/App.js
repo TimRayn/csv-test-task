f147d28147e5c4dac0f4d2928cfab7ed96a25274
dima <maxfoxlex@gmail.com>
2021-03-31 15:32:49 +0300

    added data handle

--- src/App.js
+++ src/App.js
@@ -1,47 +1,65 @@
 import './app.scss';
-import React from 'react';
+import React, { useState } from 'react';
 import CSVReader from 'react-csv-reader';
+import Table from './table';
 
 
 function App() {
+
+  const [data, setData] = useState(null);
+
   const papaparseOptions = {
     header: true,
-    dynamicTyping: true,
-    skipEmptyLines: true,
+    skipEmptyLines: 'greedy',
     transformHeader: header =>
       header
         .toLowerCase()
         .replace(/\W/g, '_')
   };
 
-  function handleForce(file, fileInfo) {
-
-    console.log(file);
-    console.log(fileInfo);
+  function da() {
+    console.log(data);
   }
 
-  function handleDarkSideForce(error) {
-    console.log(error);
+  function checkDuplicates(dataArray) {
+    for (const item of dataArray) {
+      const match = dataArray.find(elem => elem !== item && (elem.phone === item.phone || elem.email.toLowerCase() === item.email.toLowerCase()))
+      if(match) {
+        item.duplicate_with = match.id;
+        match.duplicate_with = item.id;
+      }
+    }
+
+    return dataArray;
   }
 
-  
+  function fileHandle(data, fileInfo) {
+    const newData = data.map(item => {
+      item.id = (data.indexOf(item) + 1).toString();
+      item.duplicate_with = '-';
+      return item;
+    });
+
+    checkDuplicates(newData);
+
+    setData(newData);
+  }
+
+
+
 
   return (
     <div className="app">
       <CSVReader
-        strict
-
-        className="csv-reader-input"
-        onFileLoaded={handleForce}
-        onError={handleDarkSideForce}
         parserOptions={papaparseOptions}
-        inputId="ObiWan"
-        inputName="ObiWan"
-        inputStyle={{ }}
+        onFileLoaded={fileHandle}
       />
       <div className='result-wrapper'>
-
+        <button onClick={da}></button>
       </div>
+      {data ?
+      <Table array={data}/>
+      : null}
     </div>
   );
 }
