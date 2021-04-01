667dd6e072ef7cbffeaeb43661214a05ccc98e79
dima <maxfoxlex@gmail.com>
2021-04-01 13:56:59 +0300

    complete parsing function and styles

--- src/app.js
+++ src/app.js
@@ -2,11 +2,19 @@ import './app.scss';
 import React, { useState } from 'react';
 import CSVReader from 'react-csv-reader';
 import Table from './table';
+import moment from 'moment';
 
+function ErrorComponent({ errorText }) {
+  return (
+    <div className='error'>
+      {errorText}
+    </div>
+  );
+}
 
 function App() {
-
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
   const papaparseOptions = {
     header: true,
@@ -23,13 +31,11 @@ function App() {
       const matchPhone = dataArray.find(elem => elem !== item && elem.phone === item.phone);
       if (matchPhone) {
         item.duplicate_with = matchPhone.id;
-        matchPhone.duplicate_with = item.id;
         item.bad_data.push('phone');
       }
       const matchEmail = dataArray.find(elem => elem !== item && elem.email.toLowerCase() === item.email.toLowerCase());
       if (matchEmail) {
         item.duplicate_with = matchEmail.id;
-        matchEmail.duplicate_with = item.id;
         item.bad_data.push('email');
       }
     }
@@ -40,8 +46,8 @@ function App() {
       // cut spaces and check for empty fields
       for (const key in obj) {
         if (key !== 'bad_data') obj[key] = obj[key].trim();
-        if (obj[key] === '') {
-          obj[key] = 'NO DATA';
+        if (obj[key] === '' && ['full_name', 'phone', 'email'].includes(key)) {
+          setError(`Row with id ${obj.id} has empty required field!`);
           obj.bad_data.push(key);
         }
       }
@@ -57,38 +63,58 @@ function App() {
 
       // parse yearly income
       obj.yearly_income = Number(obj.yearly_income).toFixed(2);
-      if (obj.yearly_income === "NaN") obj.yearly_income = "NO DATA";
-      else if (obj.yearly_income < 0 || obj.yearly_income > 1000000)
+      if (obj.yearly_income < 0 || obj.yearly_income > 1000000)
         obj.bad_data.push('yearly_income');
-      
 
       // parse license states
       // не впевнений,чи правильно зрозумів завдання
       parseStates(obj.license_states);
-      if(obj.license_states === '') obj.bad_data.push('license_states');
+      if (obj.license_states === '') obj.bad_data.push('license_states');
 
       // parse expiration date
       parseDate(obj.expiration_date, obj.bad_data);
+
+      // parse phone
+      obj.phone = '+1' + obj.phone.slice(-10);
+      if (obj.phone.lenght < 12 || !obj.phone.match(/^\+\d/)) obj.bad_data.push('phone');
+
+      //parse has children
+      if (obj.has_children === '') obj.has_children = 'FALSE';
+      if (!['TRUE', 'FALSE'].includes(obj.has_children)) obj.bad_data.push('has_children');
+
+      // parse license number
+      if (!obj.license_number.match(/\w{6}/)) obj.bad_data.push("license_number");
+
+
     });
   }
 
-  function parseDate(date, bad_data){
-    
+  function parseDate(date, bad_data) {
+    if (!(moment(date, 'YYYY-MM-DD', true).isValid() ||
+      moment(date, 'MM/DD/YYYY', true))) bad_data.push('expiration_date');
   }
 
   function parseStates(states) {
     states.split('|').map(state => {
       if (!state.includes(',')) return state;
       return state.split(',').map(shortState => {
-        if (shortState.lenght < 3) return shortState;
+        if (shortState.length < 3) return shortState;
         return '';
       }).join('');
     }).join(', ');
   }
 
-  function fileHandle(data, fileInfo) {
-    const newData = data.map(item => {
-      item.id = (data.indexOf(item) + 1).toString();
+  function fileHandle(fileData, fileInfo) {
+    console.log(fileInfo);
+    if (fileInfo.name.slice(-4) !== '.csv') {
+      setData(null);
+      setError(true);
+      console.log(data);
+      return;
+    }
+    setError(null);
+    const newData = fileData.map(item => {
+      item.id = (fileData.indexOf(item) + 1).toString();
       item.duplicate_with = '-';
       item.bad_data = [];
       return item;
@@ -100,17 +126,19 @@ function App() {
     setData(newData);
   }
 
-
-
-
   return (
     <div className='app'>
       <CSVReader
+        cssClass='csv-reader-input'
         parserOptions={papaparseOptions}
         onFileLoaded={fileHandle}
+        label='Import users'
       />
       {data ?
         <Table array={data} />
+        : null}
+      {error ?
+        <ErrorComponent errorText={error} />
         : null}
     </div>
   );
